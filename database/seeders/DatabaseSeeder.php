4f7b68ec13d19a0b8d2850199fc4f948715bce20
San Ikhsan <san.projects@gdrive.biz.id>
2023-05-29 20:04:22 +0700

    SubscriptionPlan Model, Migration and Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\SubscriptionPlan;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -21,6 +23,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserRoleSeeder::class,
             UserSeeder::class,
+            SubscriptionPlan::class,
         ]);
     }
 }
---
+++ database/seeders/SubscriptionPlanTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SubscriptionPlan;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SubscriptionPlanTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $subscriptionPlans = [
+            [
+                'name' => 'Basic',
+                'price' => 299000,
+                'active_period_in_months' => 3,
+                'features' => json_encode(['Unlock 10 basic movies', 'Support 24/7 ready', 'Up to 3 users'])
+            ],
+            [
+                'name' => 'Premium',
+                'price' => 899000,
+                'active_period_in_months' => 3,
+                'features' => json_encode(['Unlock 200 awards movies', '180 subtitles available', 'iOS, Android, TV', 'Offline Mode', 'Up to 20 users', 'Support 24/7 ready'])
+            ]
+        ];
+
+        SubscriptionPlan::insert($subscriptionPlans);
+    }
+}
